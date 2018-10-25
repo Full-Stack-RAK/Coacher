dd68c2e0b5afe575939ede2234df6617f0079996
Kenny <ksperkins.ksp@gmail.com>
2018-10-24 23:10:35 -0700

    added some db seed data

--- server/models/User.js
+++ server/models/User.js
@@ -7,6 +7,6 @@ const userSchema = new Schema({
   date: { type: Date, default: Date.now }
 });
 
-const User = mongoose.model("Book", userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
