479022ed5365492d6d760027f44f27668fd26254
randy <randyaakhus@gmail.com>
2018-10-26 10:48:02 -0700

    utils updated

--- src/utils/userAPI.js
+++ src/utils/userAPI.js
@@ -1,20 +1,19 @@
 import axios from "axios";
-
+const url = 'http://192.168.1.182:3210/api/users';
 export default {
 // use these functions on the correct pages to get what you want done
   getUsers: function() {
-    return axios.get("/api/users");
+    return axios.get(url);
   },
 
   getUser: function(id) {
-    return axios.get("/api/users/" + id);
+    return axios.get(url + id);
   },
 
   deleteUser: function(id) {
-    return axios.delete("/api/users/" + id);
+    return axios.delete(url + id);
   },
-  
   saveUser: function(userData) {
-    return axios.post("/api/users", userData);
+    return axios.post(url, userData)
   }
-};
+}
